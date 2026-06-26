a96a9014086524a2c9e9fc256e6747b32ce54d66
Isaac Hernandez <isaac.ghc02@gmail.com>
2026-06-26 17:58:33 -0600

    ALU instructions tested

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> // memcpy
+#include <time.h>
 
 #define NNN(instr) ((uint16_t)(instr & 0x0FFF))
 #define N(instr) ((uint8_t)(instr & 0x000F))
@@ -12,12 +13,12 @@
 uint16_t fetch(uint16_t pc, uint8_t *mem);
 void print_regs(uint8_t *regs);
 void print_mem(uint8_t *mem, size_t n);
-void decode(uint16_t instruction);
-void execute(void);
 
 int main() {
 
   uint16_t pc; // 12 bits for 4096 memory locations
+
+  srand(time(NULL));
 
   // Memoria y registros
   uint16_t i_reg;
@@ -27,8 +28,16 @@ int main() {
 
   // Test Program
   uint8_t tests[] = {
-    0x65, 0x83,
-    0x75, 0x01
+    0x65, 0x83, // LD Vx, byte
+    0x66, 0xF0, // LD Vx, byte
+    0x84, 0x50, // LD Vx, Vy
+    0x75, 0x01, // ADD Vx, byte
+    0x87, 0x61, // OR Vx, Vy
+    0x87, 0x52, // AND Vx, Vy
+    0x85, 0x44, // ADD Vx, Vy
+    0x84, 0x55, // SUB Vx, Vy
+    0x87, 0x56, // SHR Vx
+    0x87, 0x5E, // SHL Vx
   };
   memcpy(&mem[0x200], tests, sizeof(tests));
   print_mem(mem, sizeof(tests));
@@ -40,90 +49,94 @@ int main() {
   print_regs(v_regs);
   printf("\n");
 
-  for (int i = 0; i < 2; i++) {
+  for (int i = 0; i < sizeof(tests)/2; i++) {
 
     // Fetch instruction
     uint8_t opcode_high = mem[pc];
     uint8_t opcode_low = mem[pc + 1];
-    uint16_t instr = (opcode_high << 8) | opcode_low;
+    uint16_t op = (opcode_high << 8) | opcode_low;
 
-    printf("Opcode: %4x\n", instr);
+    printf("Opcode: %4x\n", op);
 
     // Decode and execute
-    uint16_t b1 = (instr & 0xF000);
-    uint16_t b2 = (instr & 0x0F00);
-    uint16_t b3 = (instr & 0x00F0);
-    uint16_t b4 = (instr & 0x000F);
+    uint16_t b1 = (op & 0xF000);
+    uint16_t b2 = (op & 0x0F00);
+    uint16_t b3 = (op & 0x00F0);
+    uint16_t b4 = (op & 0x000F);
 
     if (b1 == 0x6000) {
-      // (6xkk) Set Vx = kk
-      v_regs[X(instr)] = KK(instr);
+      // (6xkk) LD, Vx, byte
+      v_regs[X(op)] = KK(op);
 
     } else if (b1 == 0x7000) {
-      // (7xkk) Set Vx = Vx + kk
-      v_regs[X(instr)] += KK(instr);
+      // (7xkk) ADD Vx, byte
+      v_regs[X(op)] += KK(op);
 
     } else if (b1 == 0x8000) {
 
       switch (b4) {
       case 0x0000:
         // (8xy0) LD Vx, Vy
-        v_regs[X(instr)] = v_regs[Y(instr)];
+        v_regs[X(op)] = v_regs[Y(op)];
         break;
 
       case 0x0001:
         // (8xy1) OR Vx, Vy
-        v_regs[X(instr)] |= v_regs[Y(instr)];
+        v_regs[X(op)] |= v_regs[Y(op)];
         break;
 
       case 0x0002:
         // (8xy2) AND Vx, Vy
-        v_regs[X(instr)] &= v_regs[Y(instr)];
+        v_regs[X(op)] &= v_regs[Y(op)];
         break;
 
       case 0x0003:
         // (8xy3) XOR Vx, Vy
-        v_regs[X(instr)] ^= v_regs[Y(instr)];
+        v_regs[X(op)] ^= v_regs[Y(op)];
         break;
 
       case 0x0004:
         // (8xy4) ADD Vx, Vy
-        v_regs[0xF] =
-            ((uint16_t)(v_regs[X(instr)] + (uint16_t)v_regs[Y(instr)])) &
-            0xF0; // VF = carry
-        v_regs[X(instr)] += v_regs[Y(instr)];
+        v_regs[0xF] = (((uint16_t)(v_regs[X(op)] + (uint16_t)v_regs[Y(op)])) & 0xFF00) > 0; // VF = carry
+        v_regs[X(op)] += v_regs[Y(op)];
         break;
 
       case 0x0005:
         // (8xy5) SUB Vx, Vy
-        v_regs[0xF] = X(instr) > Y(instr); // VF = Not borrow
-        v_regs[X(instr)] -= v_regs[Y(instr)];
+        v_regs[0xF] = X(op) > Y(op); // VF = Not borrow
+        v_regs[X(op)] -= v_regs[Y(op)];
         break;
 
       case 0x0006:
         // (8xy6) SHR Vx {, Vy}
-        v_regs[0xF] = v_regs[X(instr)] & 0x01; // VF = LSB shifted
-        v_regs[X(instr)] >>= 1;
+        v_regs[0xF] = v_regs[X(op)] & 0x01; // VF = LSB shifted
+        v_regs[X(op)] >>= 1;
         break;
 
       case 0x0007:
         // (8xy7) SUBN Vx, Vy
-        v_regs[0xF] = Y(instr) > X(instr); // VF = Not borrow
-        v_regs[X(instr)] = Y(instr) - X(instr);
+        v_regs[0xF] = Y(op) > X(op); // VF = Not borrow
+        v_regs[X(op)] = Y(op) - X(op);
         break;
 
       case 0x000E:
         // (8xyE) SHL Vx {, Vy}
-        v_regs[0xF] = v_regs[X(instr)] & 0x80; // VF = MSB shifted
-        v_regs[X(instr)] <<= 1;
+        v_regs[0xF] = v_regs[X(op)] & 0x80; // VF = MSB shifted
+        v_regs[X(op)] <<= 1;
         break;
 
       default:
-        printf("OPCODE:%4X inexistent\n", instr);
+        printf("OPCODE:%4X inexistent\n", op);
         break;
       }
+
+    } else if (b1 == 0xC000) {
+      // (Cxkk) RND Vx, byte
+      uint8_t rnd = rand() % 256;
+      v_regs[X(op)] = rnd & KK(op);
+
     } else {
-      printf("OPCODE:%4X not implemented yet\n", instr);
+      printf("OPCODE:%4X not implemented yet\n", op);
     }
 
     printf("RESULTADO:\n");
@@ -138,7 +151,7 @@ int main() {
 
 void print_regs(uint8_t *regs) {
   printf("-----------------------------\n");
-  for (int i = 0; i < 15; i++) {
+  for (int i = 0; i < 16; i++) {
     printf("V%1x: %02X\n", i, regs[i]);
   }
 }
